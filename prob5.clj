64ed6e0fa01dc7d262f0225f76651aa9782cb11c
Randy Henne <rmhenne@gmail.com>
2016-05-19 16:41:01 -0700

    Got problem 5 working

--- prob5.clj
+++ prob5.clj
@@ -3,6 +3,7 @@
 ;;What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 (require 'Factors)
+(import 'System.Math)
 
 
 (defn DivTimes [nn ii]
@@ -12,9 +13,31 @@
       cnt
       (recur (inc cnt) (/ left ii)))))
 
-(doseq [pp (Factors/PrimesUptoN 20)]
-  (let [xlist (map (fn [xx] (DivTimes xx pp) (range 2 21)))]
-    (println pp)))
+
+(let [answer 
+      (let [flist (Factors/PrimesUptoN 21)]
+        (loop [ll flist
+               ii (first ll)
+               prod 1]
+          (let [maxv (reduce max (map (fn [xx] (DivTimes xx ii)) (range 2 21)))
+                restlist (rest ll)
+                newprod (int (* prod (Math/Pow ii maxv)))]
+
+            (if (zero? (count restlist))
+              newprod
+              (recur restlist (first restlist) newprod)))))]
+
+  (println "**** Answer = " answer)
+  
+  (doseq [xx (range 2 21)]
+    (assert (zero? (mod answer xx)))))
+
+      
+
+
+
+
+
 
 
 
