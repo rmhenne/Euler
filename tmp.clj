5128a8847395a224866b5be99d2229d0da8e6a2a
Randy Henne <rmhenne@gmail.com>
2016-05-25 15:13:03 -0700

    added two ints to my playground file tmp.clj

--- tmp.clj
+++ tmp.clj
@@ -10,3 +10,16 @@
           nil
           (let [rl (rest ll)]
             (recur (inc cnt) rl (first rl))))))))
+
+(defn twoInts [ target llist ]
+  (cond (empty? llist) nil
+        (= (count llist) 1) (let [vv (first llist)]
+                              (if (= (* 2 vv) target) (list vv vv)))
+        :else (loop [cache #{} vv (first llist) ll (rest llist)]
+                (let [cc (- target vv)]
+                  (if (contains? cache cc)
+                    (list vv cc)
+                    (if (empty? ll)
+                      nil
+                      (recur (conj cache vv) (first ll) (rest ll))))))))
+    
